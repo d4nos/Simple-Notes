ea962f6b0e4b14b15358516a5ea362fc98b7b558
thoinvffa <83622767+thoinvffa@users.noreply.github.com>
2022-06-07 17:02:39 +0700

    remove unused code

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/WritePermissionDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/WritePermissionDialog.kt
@@ -26,22 +26,16 @@ class WritePermissionDialog(activity: Activity, val mode: Mode, val callback: ()
         val layout = if (mode == Mode.SdCard) R.layout.dialog_write_permission else R.layout.dialog_write_permission_otg
         val view = activity.layoutInflater.inflate(layout, null)
 
-        val glide = Glide.with(activity)
-        val crossFade = DrawableTransitionOptions.withCrossFade()
         when (mode) {
             Mode.Otg -> {
                 view.write_permissions_dialog_otg_text.setText(R.string.confirm_usb_storage_access_text)
-                glide.load(R.drawable.img_write_storage_otg).transition(crossFade).into(view.write_permissions_dialog_otg_image)
             }
             Mode.SdCard -> {
-                glide.load(R.drawable.img_write_storage).transition(crossFade).into(view.write_permissions_dialog_image)
-                glide.load(R.drawable.img_write_storage_sd).transition(crossFade).into(view.write_permissions_dialog_image_sd)
             }
             is Mode.OpenDocumentTreeSDK30 -> {
                 val humanizedPath = activity.humanizePath(mode.path)
                 view.write_permissions_dialog_otg_text.text =
                     Html.fromHtml(activity.getString(R.string.confirm_storage_access_android_text_specific, humanizedPath))
-                glide.load(R.drawable.img_write_storage_sdk_30).transition(crossFade).into(view.write_permissions_dialog_otg_image)
 
                 view.write_permissions_dialog_otg_image.setOnClickListener {
                     dialogConfirmed()
@@ -49,7 +43,6 @@ class WritePermissionDialog(activity: Activity, val mode: Mode, val callback: ()
             }
             Mode.CreateDocumentSDK30 -> {
                 view.write_permissions_dialog_otg_text.text = Html.fromHtml(activity.getString(R.string.confirm_create_doc_for_new_folder_text))
-                glide.load(R.drawable.img_write_storage_create_doc_sdk_30).transition(crossFade).into(view.write_permissions_dialog_otg_image)
 
                 view.write_permissions_dialog_otg_image.setOnClickListener {
                     dialogConfirmed()
